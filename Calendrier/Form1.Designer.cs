530cf8d0b854b186169a525368fdb163ed3c6153
Gabriel CHARIB POLLS <gcharibpolls@helb-prigogine.be>
2021-10-17 22:33:24 +0200

    rajouter labels jours

--- Calendrier/Form1.Designer.cs
+++ Calendrier/Form1.Designer.cs
@@ -1,17 +1,16 @@
-﻿
-namespace Calendrier
+﻿namespace Calendrier
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Variable nécessaire au concepteur.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Nettoyage des ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,71 +20,74 @@ namespace Calendrier
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Code généré par le Concepteur Windows Form
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonPrécedent = new System.Windows.Forms.Button();
-            this.buttonQuiter = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.labelMoisEnCours = new System.Windows.Forms.Label();
+            this.btNext = new System.Windows.Forms.Button();
+            this.btQuit = new System.Windows.Forms.Button();
+            this.btPrevious = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // buttonPrécedent
-            // 
-            this.buttonPrécedent.Location = new System.Drawing.Point(11, 205);
-            this.buttonPrécedent.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
-            this.buttonPrécedent.Name = "buttonPrécedent";
-            this.buttonPrécedent.Size = new System.Drawing.Size(81, 22);
-            this.buttonPrécedent.TabIndex = 0;
-            this.buttonPrécedent.Text = "< Précedent";
-            this.buttonPrécedent.UseVisualStyleBackColor = true;
-            this.buttonPrécedent.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // buttonQuiter
-            // 
-            this.buttonQuiter.Location = new System.Drawing.Point(282, 398);
-            this.buttonQuiter.Name = "buttonQuiter";
-            this.buttonQuiter.Size = new System.Drawing.Size(75, 23);
-            this.buttonQuiter.TabIndex = 1;
-            this.buttonQuiter.Text = "Quitter";
-            this.buttonQuiter.UseVisualStyleBackColor = true;
-            this.buttonQuiter.Click += new System.EventHandler(this.buttonQuiter_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(562, 205);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Suivant >";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // labelMoisEnCours
             // 
             this.labelMoisEnCours.AutoSize = true;
-            this.labelMoisEnCours.Location = new System.Drawing.Point(255, 9);
+            this.labelMoisEnCours.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.labelMoisEnCours.Location = new System.Drawing.Point(301, 25);
             this.labelMoisEnCours.Name = "labelMoisEnCours";
-            this.labelMoisEnCours.Size = new System.Drawing.Size(81, 15);
-            this.labelMoisEnCours.TabIndex = 3;
-            this.labelMoisEnCours.Text = "mois en cours";
+            this.labelMoisEnCours.Size = new System.Drawing.Size(90, 13);
+            this.labelMoisEnCours.TabIndex = 0;
+            this.labelMoisEnCours.Text = "Le mois en cours:";
+            this.labelMoisEnCours.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btNext
+            // 
+            this.btNext.Location = new System.Drawing.Point(634, 254);
+            this.btNext.Name = "btNext";
+            this.btNext.Size = new System.Drawing.Size(84, 43);
+            this.btNext.TabIndex = 2;
+            this.btNext.Text = "Suivant >";
+            this.btNext.UseVisualStyleBackColor = true;
+            this.btNext.Click += new System.EventHandler(this.btNext_Click);
+            // 
+            // btQuit
+            // 
+            this.btQuit.Location = new System.Drawing.Point(279, 518);
+            this.btQuit.Name = "btQuit";
+            this.btQuit.Size = new System.Drawing.Size(127, 43);
+            this.btQuit.TabIndex = 3;
+            this.btQuit.Text = "Quitter";
+            this.btQuit.UseVisualStyleBackColor = true;
+            this.btQuit.Click += new System.EventHandler(this.btQuit_Click);
+            // 
+            // btPrevious
+            // 
+            this.btPrevious.Location = new System.Drawing.Point(10, 254);
+            this.btPrevious.Name = "btPrevious";
+            this.btPrevious.Size = new System.Drawing.Size(91, 43);
+            this.btPrevious.TabIndex = 4;
+            this.btPrevious.Text = "< Précédent";
+            this.btPrevious.UseVisualStyleBackColor = true;
+            this.btPrevious.Click += new System.EventHandler(this.btPrevious_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(671, 447);
+            this.ClientSize = new System.Drawing.Size(737, 588);
+            this.Controls.Add(this.btPrevious);
+            this.Controls.Add(this.btQuit);
+            this.Controls.Add(this.btNext);
             this.Controls.Add(this.labelMoisEnCours);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.buttonQuiter);
-            this.Controls.Add(this.buttonPrécedent);
-            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.IsMdiContainer = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calendrier";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,10 +95,10 @@ namespace Calendrier
 
         #endregion
 
-        private System.Windows.Forms.Button buttonPrécedent;
-        private System.Windows.Forms.Button buttonQuiter;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label labelMoisEnCours;
+        private System.Windows.Forms.Button btNext;
+        private System.Windows.Forms.Button btQuit;
+        private System.Windows.Forms.Button btPrevious;
     }
 }
 
--- Calendrier/Form1.cs
+++ Calendrier/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +15,6 @@ namespace Calendrier
 {
     public partial class Form1 : Form
     {
-        /// <summary>
-        /// block 1: les variables 
-        /// </summary>
-
         private const int MaxRow = 6;
         private const int MaxCol = 7;
         Button[,] buttons = new Button[MaxRow, MaxCol];
@@ -28,25 +26,32 @@ namespace Calendrier
         int cptClick = 0;
         int cptClickP = 0;
 
-
-
         public Form1()
         {
             InitializeComponent();
             createMonthButtons();
             daysLabels();
 
-        }
 
-        private void currentMonth(DateTime date)
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
         {
-            labelMoisEnCours.Text = "Mois en Cours : " + date.ToString("M / yyyy");
+            fillFromDateFirstThisMonth(firstDAY(), lastDay());
+
         }
 
+        private void textCurrentMonth(DateTime date)
+        {
+            labelMoisEnCours.Text = "Mois en cours: " + date.ToString("M / yyyy");
+        }
 
+        /// <summary>
+        /// creation des boutons.remplir un tableaux de boutons 
+        /// </summary>
         private void createMonthButtons()
         {
-            
+           
             horizontal = 115;
             vertical = 80;
             for (int ligne = 0; ligne < MaxRow; ligne++)
@@ -60,50 +65,40 @@ namespace Calendrier
                     this.buttons[ligne, col].Size = new System.Drawing.Size(55, 55);
                     this.buttons[ligne, col].Text = "";
                     this.Controls.Add(buttons[ligne, col]);
-                   // this.buttons[ligne, col].Click += new EventHandler(this.button_ClickEvent);
                     horizontal += 60;
 
                 }
                 horizontal = 115;
                 vertical += 60;
             }
-            
 
         }
 
-
-        private void daysLabels()
+        private int firstDAY()
         {
-            int x;
-            int y;
-            x = 115;
-            y = 60;
-            //DateTime day=day.Date
-            CultureInfo culture = new CultureInfo("fr-BE");
-
-            DayOfWeek weekDays = culture.DateTimeFormat.FirstDayOfWeek;//monday
-
-            for (int i = 0; i < days.Length; i++)
-            {
-                this.days[i] = new Label();
-                this.days[i].AutoSize = true;
-                this.days[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                this.days[i].Location = new System.Drawing.Point(x, y);
-                this.days[i].Name = "day";
-                this.days[i].Size = new System.Drawing.Size(55, 55);
-                this.days[i].Text = weekDays.ToString();
-                this.Controls.Add(this.days[i]);
-                weekDays++;
-                x += 60;
-                if (i == 6)
-                {
-                    this.days[i].Text = DayOfWeek.Sunday.ToString();
-                }
-
-
-            }
+            // Cette méthode retourne le premier jour du MOIS COURANT et convertit les jours de la semaine en valeur int pour remplir le tableau
+            textCurrentMonth(today);
+            DateTime thisDay = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            return convertDays(thisDay);
         }
 
+
+        private int firstDayNextMonth(int cptClique)// à chaque clique ça va faire +1
+        {
+            // Cette méthode retourne le premier jour DES MOIS SUIVANTS et convertit les jours de la semaine en valeur int pour remplir le tableau
+            DateTime dayNextMonth = new DateTime(today.AddMonths(cptClique).Year, today.AddMonths(cptClique).Month, 1);
+            //MessageBox.Show("firstDayNextMonth" + dayNextMonth);
+            textCurrentMonth(dayNextMonth);//ok
+            return convertDays(dayNextMonth);
+        }
+
+        private int firstDayPrevMonth(int click)
+        {
+            DateTime dayPrevMonth = new DateTime(today.AddMonths(-click).Year, today.AddMonths(-click).Month, 1);
+            textCurrentMonth(dayPrevMonth);
+            //MessageBox.Show(" firstDayPrevMonth" + dayPrevMonth);
+            return convertDays(dayPrevMonth);
+        }
 
         private int convertDays(DateTime thisDay)
         {
@@ -138,18 +133,157 @@ namespace Calendrier
             }
             return 0;
         }
-
-
-
-
-        private void button1_Click(object sender, EventArgs e)
+        private int lastDay()
         {
+            // cette méthode permet de calculer le dernier jour du MOIS COURANT
+            DateTime dayOne = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            DateTime last = dayOne.AddMonths(1).AddDays(-1);
+            int dayMax = last.Day;//conversion de datetime en int
+            return dayMax;
+
+        }
+        private int lastDayNextMonth(int click)
+        {
+            // cette méthode permet de calculer le dernier jour du MOIS SUIVANT
+            DateTime dayNextMonth = new DateTime(today.AddMonths(click).Year, today.AddMonths(click).Month, 1);
+            DateTime last = dayNextMonth.AddMonths(1).AddDays(-1);
+            int dayMax = last.Day;
+            //MessageBox.Show("" + dayMax);
+            return dayMax;
+        }
+        private int lastDayOfPreviousMonth(int click)
+        {
+            // cette méthode premet de calculer le dernier jour du MOIS PRECEDENT
+            DateTime dayOne = new DateTime(today.AddMonths(-click).Year, today.AddMonths(-click).Month, 1);
+            DateTime last = dayOne.AddDays(-1);
+            int dayMax = last.Day;
+            //MessageBox.Show("lastDayOfPreviousMonth" + dayMax);
+            return dayMax;
+
+        }
+        private void daysLabels()
+        {
+            int x;
+            int y;
+            x = 115;
+            y = 60;
+            //DateTime day=day.Date
+            CultureInfo culture = new CultureInfo("fr-BE");
+
+            DayOfWeek weekDays = culture.DateTimeFormat.FirstDayOfWeek;//monday
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                this.days[i] = new Label();
+                this.days[i].AutoSize = true;
+                this.days[i].Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                this.days[i].Location = new System.Drawing.Point(x, y);
+                this.days[i].Name = "day";
+                this.days[i].Size = new System.Drawing.Size(55, 55);
+                this.days[i].Text = weekDays.ToString();
+                this.Controls.Add(this.days[i]);
+                weekDays++;
+                x += 60;
+                if (i == 6)
+                {
+                    this.days[i].Text = DayOfWeek.Sunday.ToString();
+                }
+
+
+            }
+        }
+
+
+
+
+
+        private void fillFromDateFirstThisMonth(int min, int max)
+        {
+            // Cette méthode permet de remplir les boutons du calendrier
+
+            int c = min;
+            int r = 0;
+            int dd = 1;
+            /*MessageBox.Show("min" + min);
+            MessageBox.Show("max" + max);*/
+            while (dd != max)
+            {
+                this.buttons[r, c].Text = dd.ToString();
+                //MessageBox.Show(" tab[ " + r + " , " + c + "  ]= " + this.buttons[r, c].Text);
+                dd++;
+                c++;
+                if (c == MaxCol)// if c==6
+                {
+                    c = 0;
+                    r++;
+
+                }
+                else if (r == MaxRow)
+                {
+                    r = 0;
+                    c++;
+                }
+
+            }
+            dd = max;
+            this.buttons[r, c].Text = dd.ToString();
+            for (int i = 0; i < MaxRow; i++)
+            {
+                for (int j = 0; j < MaxCol; j++)
+                {
+                    if (this.buttons[i, j].Text == "")
+                    {
+                        this.buttons[i, j].Enabled = false;
+                    }
+
+                }
+            }
 
         }
 
-        private void buttonQuiter_Click(object sender, EventArgs e)
+        private void clearButtons()
         {
-            //test git
+            for (int i = 0; i < MaxRow; i++)
+            {
+                for (int j = 0; j < MaxCol; j++)
+                {
+                    this.buttons[i, j].Enabled = true;
+                    this.buttons[i, j].Text = "";
+                }
+
+            }
         }
+
+
+        private void btPrevious_Click(object sender, EventArgs e)
+        {
+            cptClickP++;
+            clearButtons();
+            lastDayOfPreviousMonth(cptClickP);
+            fillFromDateFirstThisMonth(firstDayPrevMonth(cptClickP), lastDayOfPreviousMonth(cptClickP));
+
+        }
+
+        private void btNext_Click(object sender, EventArgs e)
+        {
+            cptClick++;
+            clearButtons();
+            fillFromDateFirstThisMonth(firstDayNextMonth(cptClick), lastDayNextMonth(cptClick));
+        }
+
+        private void btQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        /* public String addNoteToday()
+         {
+             // firstday retourne l annee le mois
+             //stringtoday.ToString("M / yyyy");
+             return day;
+         }*/
+
+
     }
 }
