af5d7f44ecfa437698f58920e133d0f9dc8aaeaa
gabrielcharib <charib1997@outlook.com>
2021-10-23 20:07:28 +0200

    recupere text d'un text d'un text box

--- Calendrier/Form2.Designer.cs
+++ Calendrier/Form2.Designer.cs
@@ -29,25 +29,58 @@ namespace Calendrier
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.labeRdv = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.TextBox();
+            this.buttonEnregistre = new System.Windows.Forms.Button();
+            this.buttonQuiter = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // labeRdv
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(327, 119);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.labeRdv.AutoSize = true;
+            this.labeRdv.Location = new System.Drawing.Point(334, 31);
+            this.labeRdv.Name = "labeRdv";
+            this.labeRdv.Size = new System.Drawing.Size(29, 15);
+            this.labeRdv.TabIndex = 0;
+            this.labeRdv.Text = "RDV";
+            this.labeRdv.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(186, 81);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(348, 203);
+            this.text.TabIndex = 1;
+            // 
+            // buttonEnregistre
+            // 
+            this.buttonEnregistre.Location = new System.Drawing.Point(186, 397);
+            this.buttonEnregistre.Name = "buttonEnregistre";
+            this.buttonEnregistre.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnregistre.TabIndex = 2;
+            this.buttonEnregistre.Text = "Enregistre";
+            this.buttonEnregistre.UseVisualStyleBackColor = true;
+            this.buttonEnregistre.Click += new System.EventHandler(this.buttonEnregistre_Click);
+            // 
+            // buttonQuiter
+            // 
+            this.buttonQuiter.Location = new System.Drawing.Point(458, 397);
+            this.buttonQuiter.Name = "buttonQuiter";
+            this.buttonQuiter.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuiter.TabIndex = 3;
+            this.buttonQuiter.Text = "Quiter";
+            this.buttonQuiter.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonQuiter);
+            this.Controls.Add(this.buttonEnregistre);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.labeRdv);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
@@ -57,6 +90,9 @@ namespace Calendrier
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labeRdv;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Button buttonEnregistre;
+        private System.Windows.Forms.Button buttonQuiter;
     }
 }
--- Calendrier/Form2.cs
+++ Calendrier/Form2.cs
@@ -10,6 +10,10 @@ namespace Calendrier
 {
     public partial class Form2 : Form
     {
+
+        Dictionary<DateTime, String> noteCalendrier = new Dictionary<DateTime, string>(); //dico ou on va stoque les informations 
+
+
         public Form2()
         {
             InitializeComponent();
@@ -17,6 +21,22 @@ namespace Calendrier
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void buttonEnregistre_Click(object sender, EventArgs e)
+        {
+
+            String txt = text.Text;
+            MessageBox.Show(txt);
+        // si text vide afficher message  d'ereur 
+
+        //si non vide 
+
+        //recupere date 
+        // date = cle dico 
+        // enregistre message dans dico
+        // tout enregistre
 
         }
     }
